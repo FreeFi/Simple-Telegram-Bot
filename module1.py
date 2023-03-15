ba4388e2831be642f738fb62deda015d965f017d
Rororo <40234315+FreeFi@users.noreply.github.com>
2023-03-15 13:15:06 +1100

    Add files via upload

---
+++ module1.py
@@ -0,0 +1,14 @@
+﻿
+import telebot
+
+bot = telebot.TeleBot("5847794927:AAHhV6SMQoAVJDFrZHjxWs8rJ9Nb0TBpcXE")
+
+@bot.message_handler(commands=['Старт', 'помощь'])
+def send_welcome(message):
+	bot.reply_to(message, "Привет, как дела?")
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+	bot.reply_to(message, message.text)
+
+bot.infinity_polling()
